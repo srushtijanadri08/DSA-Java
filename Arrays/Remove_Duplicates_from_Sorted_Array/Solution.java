1dd85fc71448cb1dbafc74e26f6c8093f900e814
Srushti Janadri <srushtijanadri00@gmail.com>
2026-09-01 21:11:17 +0530

    Add Remove Duplicates solution

---
+++ Arrays/Remove_Duplicates_from_Sorted_Array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        int i=1;
+        for(int j=1;j<=n-1;j++){
+            if(nums[j]!=nums[j-1]){
+                nums[i]=nums[j];
+                i++;
+                }
+                 
+        }
+        return i;
+    }
+}
