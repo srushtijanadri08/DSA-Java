9da46cdab88aae4207ccf7fe93f3cbe09c9600ff
Srushti Janadri <srushtijanadri00@gmail.com>
2026-09-06 22:10:55 +0530

    Adding Two-sum II-input array is sorted

---
+++ TwoPointer/Two_sum II-Input Array is Sorted/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int i=0;
+        int j=n-1;
+         
+        while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum>target){
+                j--;
+            }else if(sum<target){
+                i++;
+            }else{
+                return new int[]{i+1,j+1};
+            }
+        }
+        return new int[]{};
+    }
+}
