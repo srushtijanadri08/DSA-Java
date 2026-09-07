f1729789e92012f77608cff6b1f5c48e9dba1fd8
Srushti Janadri <srushtijanadri00@gmail.com>
2026-09-07 22:21:07 +0530

    Adding the Sorted Colord Solution

---
+++ TwoPointer/Sort_Colors/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        int j=0;
+        int k=n-1;
+        
+            while(j<=k){
+                if(nums[j]==0){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    i++;
+                    j++;
+                }else if(nums[j]==1){
+                    j++;
+                }else{
+                    int temp=nums[j];
+                    nums[j]=nums[k];
+                    nums[k]=temp;
+                    k--;
+                }
+            }
+        }
+    }
